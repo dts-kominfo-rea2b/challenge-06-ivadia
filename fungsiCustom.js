6b15c30d782967ff81395e31fe69bc24b85c2d37
Ivadia Elmina Patola <ivadia2401@gmail.com>
2022-06-10 10:20:12 +0800

    done challenge 6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,36 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  const readData = [];
+  const sumberData = [file1, file2, file3];
+
+  for (const sumber of sumberData) {
+    fs.readFile(sumber, function read(err, data) {
+      if (err) {
+        return console.log(err);
+      } else {
+        const file = JSON.parse(data);
+
+        if (file?.message !== undefined) {
+          readData.push(file.message.split(" ")[1]);
+        }
+
+        if (file?.length) {
+          file?.forEach((item) => {
+            if (item?.message !== undefined) {
+              readData.push(item.message.split(" ")[1]);
+            }
+            if (item?.data?.message !== undefined) {
+              readData.push(item.data.message.split(" ")[1]);
+              fnCallback(null, readData);
+            }
+          });
+        }
+      }
+    });
+  }
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
